5dfcffacd86d21c7e574ce078094c89372eaf66a
hrokit <hrk.kho@gmail.com>
2023-01-11 16:19:58 +0000

    Find Users & Find User By Id

--- api/src/routes/user.route.js
+++ api/src/routes/user.route.js
@@ -2,5 +2,7 @@ const router = require('express').Router();
 const userController = require('../controllers/user.controller');
 
 router.post('/', userController.create);
+router.get('/', userController.findAll);
+router.get('/:id', userController.findById);
 
 module.exports = router;
