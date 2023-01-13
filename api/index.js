4b4008f645e732cfc5947a8a7ad54b939205905f
hrokit <hrk.kho@gmail.com>
2023-01-13 20:46:14 +0000

    Dotenv

--- api/index.js
+++ api/index.js
@@ -1,14 +1,16 @@
 import express from 'express';
+import dotenv from 'dotenv';
 import connectDB from './src/database/db.js';
 import userRoute from './src/routes/user.route.js';
 
-connectDB();
-const app = express();
+dotenv.config();
 
+connectDB();
+
+const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-
 app.use('/user', userRoute);
 
 app.listen(port, () => console.log(`server OK, port ${port}`));
--- api/src/database/db.js
+++ api/src/database/db.js
@@ -1,14 +1,15 @@
 import mongoose from 'mongoose';
+
 mongoose.set('strictQuery', false);
 
 const connectDB = () => {
   console.log('Wait connection to db');
 
   mongoose
-    .connect(
-      'mongodb+srv://root:admin@cluster0.ziccd5y.mongodb.net/thi-code?retryWrites=true&w=majority',
-      { useNewUrlParser: true, useUnifiedTopology: true }
-    )
+    .connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
     .then(() => console.log('MondoDB Atlas connected'))
     .catch(error => console.log(error));
 };
